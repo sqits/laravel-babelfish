b728fe3008ae4db2e77cdd6c94238673c99f3e1d
Ruud Schaaphuizen <ruud@sqits.nl>
2022-10-11 14:07:16 +0200

    wip

--- packages/sqits/laravel-babelfish/resources/js/app.js
+++ packages/sqits/laravel-babelfish/resources/js/app.js
@@ -1,23 +1,23 @@
 import { createApp } from 'vue'
 import axios from 'axios';
 
-// axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-//
-// let token = document.head.querySelector('meta[name="csrf-token"]');
-//
-// if (token) {
-//     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-// }
-//
-// Vue.prototype.$http = axios.create();
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-createApp({
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
+const app = createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            // default data
         }
     }
 }).mount('#babelfish')
+
+app.config.globalProperties.$axios = axios.create();
 
 
 
