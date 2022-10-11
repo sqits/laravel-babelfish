5dd21a5aaedbc43443e26652e2be7bc0a321bf17
Roderick Martina <rick@sqits.nl>
2022-10-11 15:35:43 +0200

    migration fixes

--- packages/sqits/laravel-babelfish/src/BabelfishServiceProvider.php
+++ packages/sqits/laravel-babelfish/src/BabelfishServiceProvider.php
@@ -45,7 +45,7 @@ class BabelfishServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../database/migrations/create_languages_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_languages_table.php'),
-            __DIR__.'/../database/migrations/create_translations_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_translations_table.php'),
+            __DIR__.'/../database/migrations/create_translations_table.php.stub' => database_path('migrations/'.now()->addDay()->format('Y_m_d_His').'_create_translations_table.php'),
         ], 'babelfish-migrations');
     }
 
